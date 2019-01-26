8a0a15df3754a8969ce85fb5e3597ecf07d5e216
Caner <cnrdmrci@hotmail.com>
2019-01-26 13:47:41 +0300

    Ekleme yapıldı.
    Yeni fonksiyonlar için güncelleme.

--- Main.cs
+++ Main.cs
@@ -4,17 +4,51 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
-using ORM;
+using AttribueTanimlamalari;
+using ORMKaydet;
+using ORMSil;
+using ORMGuncelle;
+using ORMAramaYap;
 
 namespace Main
 {
     class Program
     {
-            TabloAdi tabloAdi = new TableName();
-            tabloAdi.Ad = "Caner";
-            tabloAdi.Soyad = "Demirci";
-
-            tabloAdi.VeriTabaninaKaydet();
+            veritabaninaKaydet(); // Tabloya veri kaydettim. id = 5, Ad = "caner" , Soyad = "Demirci"
+            veriTabaniIdIleGuncelle(); // id ile adımın baş harfini büyük olarak güncelledim. Ad = "Caner"
+            veritabanindanVeriArayan(); // Herhangi bir alana değer atayarak arama yaptım.
+            veritabanindanSil(); // id değeri ile silme işlemi yaptım.
+            
+    }
+    
+    private void veritabaninaKaydet()
+    {
+           var tabloAdi = new TabloAdi();
+           tabloAdi.Ad = "caner";
+           tabloAdi.Soyad = "Demirci";
+           tabloAdi.VeritabaninaKaydet();
+    }
+    
+    private void veritabaniIdIleGuncelle()
+    {
+           var tabloAdi = new TableAdi();
+           tabloAdi.Id = 5;
+           tabloAdi.Ad = "Caner";
+           tabloAdi.VeritabaniIdIleGuncelle();
+    }
+    
+    private void veritabanindanVeriArayan()
+    {
+           var tabloAdi = new TableAdi();
+           tabloAdi.Ad = "Caner";
+           tabloAdi.VeritabanindanVeriArayan();
+    }
+    
+    private void veritabanindanSil()
+    {
+           var tabloAdi = new TableAdi();
+           tabloAdi.Id = 5;
+           tabloAdi.VeritabanindanSil();
     }
 
     [DbTablo("TABLO_ADI")]
